47c1732669ac269466b3f84eac755e82c345e425
alysoncampos <alyson.siqueiracampos@gmail.com>
2021-11-05 13:37:22 -0300

    @alysoncampos[Task: CREATE Spring Boot Project]

---
+++ myGameStore/src/main/java/com/myGameStore/myGameStore/MyGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.myGameStore.myGameStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameStoreApplication.class, args);
+	}
+
+}
